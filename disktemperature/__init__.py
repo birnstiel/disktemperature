8b24005d49d1a490772ce83b804f470bc41b1989
birnstiel@me.com <birnstiel@me.com>
2017-08-25 13:59:08 +0200

    fixed init file

--- disktemperature/__init__.py
+++ disktemperature/__init__.py
@@ -4,8 +4,8 @@ Initializaition file for package `disktemperature`.
 from setuptools_scm import get_version as __get_version
 __version__ = __get_version(root='..', relative_to=__file__)
 
-import constants
-import pmstracks
-import tmid
+from . import constants
+from . import pmstracks
+from . import tmid
 
 __all__ = ['constants', 'pmstracks', 'tmid']
