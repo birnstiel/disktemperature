a5a6fd5be55723ea3885c69ff473681c5c3f5828
birnstiel@me.com <birnstiel@me.com>
2017-08-25 13:58:10 +0200

    improved the init file

--- disktemperature/__init__.py
+++ disktemperature/__init__.py
@@ -1,5 +1,11 @@
 """
 Initializaition file for package `disktemperature`.
 """
-from setuptools_scm import get_version
-__version__ = get_version(root='..', relative_to=__file__)
+from setuptools_scm import get_version as __get_version
+__version__ = __get_version(root='..', relative_to=__file__)
+
+import constants
+import pmstracks
+import tmid
+
+__all__ = ['constants', 'pmstracks', 'tmid']
